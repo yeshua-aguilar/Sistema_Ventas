0e0ae0a1de21b3a7dc118615b33a5bedf143acd5
yeshuaaguilar <81573986+yeshua-aguilar@users.noreply.github.com>
2022-07-17 14:30:04 -0500

    Update conexion.php

--- modelos/conexion.php
+++ modelos/conexion.php
@@ -4,9 +4,9 @@ class Conexion{
 
 	static public function conectar(){
 
-		$link = new PDO("mysql:host=localhost;dbname=database",
+		$link = new PDO("mysql:host=containers-us-west-69.railway.app;dbname=railway",
 			            "root",
-			            "");
+			            "0SlS0ot5lnTqM6MEH9wX");
 
 		$link->exec("set names utf8");
 
