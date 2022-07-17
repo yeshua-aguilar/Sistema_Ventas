435d77862e4c0ef0690e5d53fb74ef127744ab9a
yeshuaaguilar <81573986+yeshua-aguilar@users.noreply.github.com>
2022-07-17 15:26:21 -0500

    Update conexion.php

--- modelos/conexion.php
+++ modelos/conexion.php
@@ -4,7 +4,7 @@ class Conexion{
 
 	static public function conectar(){
 
-		$link = new PDO("mysql:host=localhost;dbname=railway",
+		$link = new PDO("mysql:host=containers-us-west-83.railway.app;dbname=railway",
 			            "root",
 			            "BtPQbAjCGtxbKcCkAbOg");
 
